aa787d56b2713ba1cda5ea187211089b597185b4
Tyler May <tylermay@Tylers-MacBook-Air.local>
2023-12-01 08:04:53 -0700

    Finished dynamic data lab

---
+++ iTunesSearch/ItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCollectionViewCell.swift
+//  iTunesSearch
+//
+//  Created by Tyler May on 11/30/23.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell, ItemDisplaying {
+    
+    @IBOutlet var itemImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var detailLabel: UILabel!
+    
+    
+}
